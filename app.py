0b3b7e572dc5ef96036a0c59559a74c83a539a44
Yurii <yhospodaryshyn@amcbridge.com>
2025-12-11 15:35:02 +0200

    update

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ APP_VERSION = os.environ.get("APP_VERSION", "unknown")
 def hello():
     return f"Hello from Flask-1 inside Docker! Version: {APP_VERSION}"
 
-@app.route("/app-1/health")
-def health():
-    return {'status': 'healthy'}, 200
+# @app.route("/app-1-health")
+# def health():
+#     return {'status': 'healthy'}, 200
 
-@app.route("/app-1/ready")
-def ready():
-    # Check database connection, etc.
-    return {'status': 'ready'}, 200
+# @app.route("/app-1-ready")
+# def ready():
+#     # Check database connection, etc.
+#     return {'status': 'ready'}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
